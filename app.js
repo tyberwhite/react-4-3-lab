7c303ee0deedcf5a26ff424c71fb523eb45e0a39
Jesse <zerocool@Jesses-MBP.hsd1.ma.comcast.net>
2022-12-12 09:52:07 -0500

    easy going answered

---
+++ app.js
@@ -0,0 +1,26 @@
+// Monday, Dec 12, 2022
+// React 4.3 -- Lab
+// J.T.W.
+
+//////////////////////////////////////////
+// Arrays, Loops, and Conditional Reps
+//////////////////////////////////////////
+
+//////////////////////
+// Part I: HTML & CSS
+//////////////////////
+
+// Rank your comfort: 5
+
+///////////////////////////////
+// Part II: JavaScript Reps
+///////////////////////////////
+
+// Write a for loop that will log the numbers 1 through 20.
+function logTheNums() {
+  for (let i = 1; i <= 20; i++) {
+    console.log(i);
+  }
+}
+
+// logTheNums();
